aee55720adb2c4aca78f3c075366801a27e1e499
Josh <josh12365@gmail.com>
2022-04-25 21:26:00 +0800

    add header

--- product/product.py
+++ product/product.py
@@ -14,7 +14,9 @@ print(product)
 for p in product:
 	print(p[0], '的價格是', p[1])
 
-with open('product.csv', 'w') as f:
+with open('product.csv', 'w', encoding='utf-8') as f: 
+#encoding='utf-8'世界共通的編碼
+	f.write('商品,價格\n')
 	for p in product:
 		f.write(p[0] + ',' + p[1] + '\n')
 #csv檔通常會用逗點做區隔

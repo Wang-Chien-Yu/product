95a1fe0543a2d0686902b2964643665142539eb8
Josh <josh12365@gmail.com>
2022-04-25 20:59:52 +0800

    add write

--- product/product.py
+++ product/product.py
@@ -13,3 +13,9 @@ print(product)
 
 for p in product:
 	print(p[0], '的價格是', p[1])
+
+with open('product.csv', 'w') as f:
+	for p in product:
+		f.write(p[0] + ',' + p[1] + '\n')
+#csv檔通常會用逗點做區隔
+#寫入一定要字串

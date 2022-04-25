4d8e7197634067717ee2da3934a5b80472921762
Josh <josh12365@gmail.com>
2022-04-25 21:52:57 +0800

    add read list

--- product/product.py
+++ product/product.py
@@ -1,4 +1,10 @@
 product = []
+#讀取清單
+# with open('product.csv', 'r', encoding='utf-8') as f:
+# 	for line in f:
+# 		name, price = line.strip().split(',')
+# 		product.append([name, price])
+# print(product)
 
 while True:
 	name = input('請輸入商品名稱: ')
@@ -16,6 +22,7 @@ for p in product:
 
 with open('product.csv', 'w', encoding='utf-8') as f: 
 #encoding='utf-8'世界共通的編碼
+
 	f.write('商品,價格\n')
 	for p in product:
 		f.write(p[0] + ',' + p[1] + '\n')
